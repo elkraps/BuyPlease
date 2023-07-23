c5a19229a669d2993afbc30f55b3d82e3128d576
elkraps <dobroisreal@mail.ru>
2023-07-23 08:51:05 +0300

    Initial Commit

---
+++ BuyPlease/BuyPleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyPleaseApp.swift
+//  BuyPlease
+//
+//  Created by Vlad Dzirko on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BuyPleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
